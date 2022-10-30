a592c06c8c71ec1316706f733f01a7537f0c1fb0
Olga Mirete <olgamirete@gmail.com>
2022-10-30 18:52:58 +0100

    implemented react router <Link /> component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App";
 import "./custom.scss";
+import InterestingChannels from "./sections/InterestingChannels";
+import NewsChannels from "./sections/NewsChannels";
+import Search from "./sections/Search";
+
+const childRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <NewsChannels />,
+    index: true,
+  },
+  {
+    path: "/interesting-channels",
+    element: <InterestingChannels />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: childRoutes,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
